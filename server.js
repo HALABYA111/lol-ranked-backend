0097fbe7b8fddb250f48c199502255b56ea2a270
HALABYA111 <160992515+HALABYA111@users.noreply.github.com>
2026-01-08 00:32:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -153,6 +153,8 @@ app.get("/rank", async (req, res) => {
    START SERVER
 ================================ */
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Backend running on port ${PORT}`)
-);
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
+});
+
